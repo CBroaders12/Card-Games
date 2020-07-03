e61d29c11d94e83fc0a4d3fe75c69427cc9efe4a
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-07-03 09:14:17 -0500

    Condensed round logic into play_hand()

--- Card_Games/war.py
+++ Card_Games/war.py
@@ -36,54 +36,75 @@ def war(player1, player2, deck):
     
     round = 0
 
-    while player1.hand and player2.hand:
-        round += 1
+    while player1.hand and player2.hand: #While there are still cards in the deck
+        round += 1  #Increment round counter
         print("**********")
         print("Round {}!".format(round))
         print("-----------")
+        
         cards_played = []
-        player1_card = player1.play_card()
-        player2_card = player2.play_card()
-        cards_played += [player1_card, player2_card]
-
-        print("%s plays the %s" % (player1.name, player1_card))
-        print("%s plays the %s" % (player2.name, player2_card))
-
-        if card_values[player1_card.value] > card_values[player2_card.value]:
-            print("-----------------")
-            print("%s takes the hand" % (player1.name))
-            print("-----------------")
-            player1.hand += cards_played
+        play_hand(player1, player2, cards_played)
         
-        elif card_values[player1_card.value] < card_values[player2_card.value]:
-            print("-----------------")
-            print("%s takes the hand" % (player2.name))
-            print("-----------------")
-            player2.hand += cards_played
-        
-        else:
-            tie_breaker(player1, player2)
-        
+        #Check how many cards each player has
         print("{} has {} cards".format(player1.name, len(player1.hand)))
         print("{} has {} cards".format(player2.name, len(player2.hand)))
         print("*******************\n")
     
+    if player1.hand:
+        print("** {} wins after {} rounds! **".format(player1.name, round))
+    
+    else:
+        print("** {} wins after {} rounds! **".format(player2.name, round))
 
 # Function for if there is a tie takes in 2 players as input
-def tie_breaker(player1, player2):
+def tie_breaker(player1, player2, cards_played=None):
     print("Tie breaker time!")
     print("----------------")
     
-    cards_played = []
+    if not cards_played:
+        cards_played = []
 
-    for x in range(3):
-        cards_played.append(player1.play_card())
-        cards_played.append(player2.play_card())
+    if len(player1.hand) < 4:
+        for x in range(len(player1.hand) - 1):
+            cards_played.append(player1.play_card())
     
+    elif len(player2.hand) < 4:
+        for x in range(len(player2.hand) - 1):
+            cards_played.append(player2.play_card())
+
+    else:
+        for x in range(3):
+            cards_played.append(player1.play_card())
+            cards_played.append(player2.play_card())
+        
+    play_hand(player1, player2, cards_played)
+
+    return
+
+def play_hand(player1, player2, cards_played):
     player1_card = player1.play_card()
     player2_card = player2.play_card()
-    cards_played += [player1_card, player2_card]
+    cards_played += [player1_card, player2_card] #Cards placed on the table
 
+    print("%s plays the %s" % (player1.name, player1_card))
+    print("%s plays the %s" % (player2.name, player2_card))
+
+    #Winner adds the played cards to the bottom of their hand
+    if card_values[player1_card.value] > card_values[player2_card.value]:
+        print("-----------------")
+        print("%s takes the hand" % (player1.name))
+        print("-----------------")
+        player1.hand += cards_played 
+        
+    elif card_values[player1_card.value] < card_values[player2_card.value]:
+        print("-----------------")
+        print("%s takes the hand" % (player2.name))
+        print("-----------------")
+        player2.hand += cards_played
+        
+    #Use a tie breaker if card values are the same
+    else:
+        tie_breaker(player1, player2)
 
 #Initialize the deck and the players
 deck = Deck()
